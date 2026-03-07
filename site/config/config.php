b3aa0ed6b7c31dc740f60d6a8dd026c3e800615f
www-data <www-data@vmi3131255.contaboserver.net>
2026-03-07 23:52:20 +0100

    SEO and renaming

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+$excludeFromSitemap = ['impressum', 'datenschutz', 'cookie-einstellungen'];
+
 return [
   'debug' => false,
   'panel' => [
@@ -10,5 +12,29 @@ return [
   ],
   'jobs' => [
     'queue' => false
+  ],
+  'routes' => [
+    [
+      'pattern' => 'sitemap.xml',
+      'action'  => function () use ($excludeFromSitemap) {
+        $pages = site()->index()->filter(function ($page) use ($excludeFromSitemap) {
+          return !in_array($page->slug(), $excludeFromSitemap);
+        });
+
+        $content = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+        $content .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+        foreach ($pages as $page) {
+          $content .= "  <url>\n";
+          $content .= '    <loc>' . esc($page->url()) . "</loc>\n";
+          $content .= '    <lastmod>' . $page->modified('Y-m-d') . "</lastmod>\n";
+          $content .= '    <changefreq>' . ($page->isHomePage() ? 'monthly' : 'yearly') . "</changefreq>\n";
+          $content .= '    <priority>' . ($page->isHomePage() ? '1.0' : '0.7') . "</priority>\n";
+          $content .= "  </url>\n";
+        }
+        $content .= '</urlset>';
+
+        return new \Kirby\Http\Response($content, 'application/xml');
+      }
+    ]
   ]
 ];
